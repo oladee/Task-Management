c436dcfec309c6c9adde6b5580446706d078f957
oladee <deemajor230600@gmail.com>
2023-10-08 08:05:21 +0100

    solved issue with checking items and saving state on reload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,15 @@ const Footer = ({lightMode}) =>{
         <div>
             <div className={lightMode ? `bg-white text-black ${normal} ` : `${normal} bg-gray-800 text-white`} >
                 <p >0 item left</p>
-                <div className='flex'>
+                <div className='hidden md:flex'>
                     <p>All</p>
                     <p className='mx-2'>Active</p>
                     <p>Completed</p>
                 </div>
                 <p>Clear Completed</p>
+            </div>
+            <div className="flex md:hidden">
+
             </div>
         </div>
     )
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import crossIcon from "../assets/icon-cross.svg";
+import checkIcon from '../assets/icon-check.svg'
 import Footer from "./Footer";
 function TodoItem({ taskName, id, lightMode, setTask, task, completed }) {
 
-  const [complete,] = useState(completed)
+
+  const [complete,setComplete] = useState(completed)
 
   function toggleComplete() {
     let items = [...task]
     items.map(value => {
       if(value.id === id ){
         value.completed = !complete
+        setComplete(value.completed)
       }
+      
     })
+    
     setTask(items)
   }
 
@@ -29,8 +34,12 @@ function TodoItem({ taskName, id, lightMode, setTask, task, completed }) {
   return (
     <>
     <div className="text-white p-3 px-4 flex justify-between border-b border-slate-500 items-center">
-      <label htmlFor={taskName} >
-      <input type="checkbox" name={taskName} id="" className="cursor-pointer" onClick={toggleComplete}/>
+      <label htmlFor={taskName} className="flex items-center" >
+      {
+        complete ?
+        (<span className="checker flex"><img src={checkIcon} alt="" onClick={toggleComplete} /></span>) : (<label className="switch border border-gray-400 " onClick={toggleComplete}>
+        </label>)
+      }
       <span className={lightMode? "text-black px-3" : "text-white px-3"}>{taskName}</span>
       </label>
       <img src={crossIcon} alt="delete icon" className="cursor-pointer" onClick={handleDelete} />
