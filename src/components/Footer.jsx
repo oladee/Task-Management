a2e8e9dfb71407bc1a16fed7d1843c698e00b758
oladee <deemajor230600@gmail.com>
2023-10-16 09:50:10 +0100

    resolved missing code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,13 @@ const Footer = ({ lightMode, task, setTask }) => {
     "flex px-4 py-4 placeholder-white-500  rounded-b justify-between items-center text-[14px]";
 
 
-    function completedList(){
-      let taskCopy = [...task]
-      let newList = taskCopy.filter(x=> x.completed === true)
-      setTask(newList)
-    }
+    const newList = [...task] 
+    const itemsLeft = newList.filter(x => x.completed === false)
 
-    function notCompletedList(){
-      let taskCopy = [...task]
-      let newList = taskCopy.filter(x=> x.completed === false)
-      setTask(newList)
+    const handleClearComplete = ()=> {
+      let newlist = newList.filter(x => x.completed !== true)
+      setTask(newlist)
     }
-
   return (
     <div>
       <div
@@ -25,13 +20,13 @@ const Footer = ({ lightMode, task, setTask }) => {
             : `${normal} bg-gray-800 text-gray-400`
         }
       >
-        <p>0 item left</p>
+        <p>{itemsLeft.length} item left</p>
         <div className="hidden md:flex ">
           <NavLink to="/" className="focus:text-blue-500">All</NavLink>
           <NavLink to="/active" className="mx-2 focus:text-blue-500">Active</NavLink>
           <NavLink to="/completed" className="focus:text-blue-500" >Completed</NavLink>
         </div>
-        <p>Clear Completed</p>
+        <p onClick={handleClearComplete} >Clear Completed</p>
       </div>
       <div className="flex md:hidden justify-center border-t-[12px] border-t-gray-900 py-4">
         <NavLink to="/" className="focus:text-blue-500">All</NavLink>
