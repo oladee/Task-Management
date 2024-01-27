b4d3d01120e73fbfbfd1b315a378f8ea04ffcfcb
Oladee <deemajor230600@gmail.com>
2024-01-27 19:59:37 +0100

    solved task issues

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAQt6m3ax45res2Y4xXHKgXwzuht3ya2cA",
+  authDomain: "oladee-featuresection.firebaseapp.com",
+  projectId: "oladee-featuresection",
+  storageBucket: "oladee-featuresection.appspot.com",
+  messagingSenderId: "459117685381",
+  appId: "1:459117685381:web:a9e8da2df7cb3c3aadd7ec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
